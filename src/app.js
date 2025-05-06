b1a392c2ec8e05555a44fcd5cfbe5053e5c8e45c
Macalo1998 <110059967+Macalo1998@users.noreply.github.com>
2025-05-06 14:21:28 +0000

    ExcusesGen

--- src/app.js
+++ src/app.js
@@ -7,5 +7,22 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let who = ['The dog', 'My grandma', 'The mailman', 'My bird'];
+  let action = ['ate', 'peed', 'crushed', 'broke'];
+  let what = ['my homework', 'my phone', 'the car','my computer'];
+  let when = ['before the class', 'when I was sleeping', 'while I was exercising', 'during my lunch', 'while I was praying'];
+  function ExcusesGen(){
+    let rand = Math.floor(Math.random()*4);
+    return rand
+  }
+  function Excuses(){
+    let Excusa = [];
+    Excusa[0]= who[ExcusesGen()];
+    Excusa[1]= action[ExcusesGen()];
+    Excusa[2]= what[ExcusesGen()];
+    Excusa[3]= when[ExcusesGen()];
+    return Excusa.join(" ");
+  }
+
+  document.getElementById("excuses").innerHTML = Excuses();
 };
